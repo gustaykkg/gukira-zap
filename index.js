4f813faf844367d1f0e9fb6256c194b94cf17206
Gustavo <gustavomontiselli@gmail.com>
2022-04-06 04:36:26 -0300

    aaa

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const accountSid = 'ACc5b7280ec605ca4864450c2faf33f989'
 const authToken = '07cf6358ff5bcab87a12343722a68e82'
 const client = require('twilio')(accountSid,authToken);
 let msgDay = true
-
+const express = require('express')
+const app = express();
+app.get("/", (request, response)=>{
+  response.sendStatus(200)
+})
+app.listen(3000, () => {
+        console.log(`Exemplo app node rodando no endereço http://localhost:${3000}`)
+      });
 
 setInterval( async()=>{
 
@@ -18,7 +25,13 @@ setInterval( async()=>{
         if(now.getMonth() != past.getMonth() && now.getDate() == past.getDate() && now.getFullYear() == past.getFullYear()){
                 client.messages.create({
                 from: 'whatsapp:+14155238886',
-                body: `Cara tamo comerando aniversario de ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"} entra ai no site que se eu não tava com preguiça eu fiz algo bonitin\nhttps://gukira.netlify.app`,
+                body: `Cara tamo comerando aniversario de ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"} entra ai no site que se eu não tava com preguiça eu fiz algo bonitin\nhttps://gukira.netlify.app\n\n\nps:ainda em desenvolvimento, se recebeu a msm e não é nosso aniversario ignora`,
+                to: 'whatsapp:+5511995729292'
+                }).then(message => console.log(message.sid))
+
+          client.messages.create({
+                from: 'whatsapp:+14155238886',
+                body: `Cara tamo comerando aniversario de ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"} entra ai no site que se eu não tava com preguiça eu fiz algo bonitin\nhttps://gukira.netlify.app\n\n\nps:ainda em desenvolvimento, se recebeu a msm e não é nosso aniversario ignora`,
                 to: 'whatsapp:+5511910759773'
                 }).then(message => console.log(message.sid))
                 msgDay = false
@@ -31,7 +44,13 @@ setInterval( async()=>{
                     
             client.messages.create({
                 from: 'whatsapp:+14155238886',
-                body: `Parça esse bot ainda ta funcionando mesmo depois de ${anodiff} ${anodiff == 1 ? "ano" : "anos"} eu provavelmente não fiz nada diferente, porem entra ai\nhttps://gukira.netlify.app`,
+                body: `Parça esse bot ainda ta funcionando mesmo depois de ${anodiff} ${anodiff == 1 ? "ano" : "anos"} eu provavelmente não fiz nada diferente, porem entra ai\nhttps://gukira.netlify.app\n\n\nps:ainda em desenvolvimento, se recebeu a msm e não é nosso aniversario ignora`,
+                to: 'whatsapp:+5511995729292'
+                }).then(message => console.log(message.sid))
+
+          client.messages.create({
+                from: 'whatsapp:+14155238886',
+                body: `Cara tamo comerando aniversario de ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"} entra ai no site que se eu não tava com preguiça eu fiz algo bonitin\nhttps://gukira.netlify.app\n\n\nps:ainda em desenvolvimento, se recebeu a msm e não é nosso aniversario ignora`,
                 to: 'whatsapp:+5511910759773'
                 }).then(message => console.log(message.sid))
                 msgDay = false
@@ -40,7 +59,13 @@ setInterval( async()=>{
         else if(mesdiff != 0 && anodiff != 0){
             client.messages.create({
                 from: 'whatsapp:+14155238886',
-                body: `Se isso ta chegando mesmo com ${anodiff} ${anodiff == 1 ? "ano" : "anos"} e ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"} significa que minha promessa ainda ta de pé e eu te amo muito mesmo\nhttps://gukira.netlify.app`,
+                body: `Se isso ta chegando mesmo com ${anodiff} ${anodiff == 1 ? "ano" : "anos"} e ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"} significa que minha promessa ainda ta de pé e eu te amo muito mesmo\nhttps://gukira.netlify.app\n\n\nps:ainda em desenvolvimento, se recebeu a msm e não é nosso aniversario ignora`,
+                to: 'whatsapp:+5511995729292'
+                }).then(message => console.log(message.sid))
+
+          client.messages.create({
+                from: 'whatsapp:+14155238886',
+                body: `Cara tamo comerando aniversario de ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"} entra ai no site que se eu não tava com preguiça eu fiz algo bonitin\nhttps://gukira.netlify.app\n\n\nps:ainda em desenvolvimento, se recebeu a msm e não é nosso aniversario ignora`,
                 to: 'whatsapp:+5511910759773'
                 }).then(message => console.log(message.sid))
                 msgDay = false
